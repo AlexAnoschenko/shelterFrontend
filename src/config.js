26b4aa277428b3594a44735412258138bf547d8d
Aliaksei <=>
2022-04-11 17:44:46 +0300

    fix env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const appConfig = {
-  API_URL: process.env.REACT_APP_LOCAL_BACKEND,
+  API_URL: process.env.REACT_APP_BACKEND,
+  SOCKET_URL: process.env.REACT_APP_BACKEND_SOCKET,
 };
 export default appConfig;
